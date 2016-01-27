4cd2b4f026e998c5947fefaa68298f69d9978bb2
Nicolas Marchildon <nicolas@marchildon.net>
2016-01-26 22:38:43 -0500

    Add WebSocket server and client for displaying the new data after an event.

--- app.js
+++ app.js
@@ -24,5 +24,48 @@ var http = require('http');
 var port = config.port || '3000';
 app.set('port', port);
 var server = http.createServer(app);
+var WebSocketServer = require('websocket').server;
+var wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+var connectedClients = [];
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+function publishData(connection) {
+  connection.sendUTF(JSON.stringify(dashboard.getData()));
+}
+wsServer.on('request', function(request) {
+  try {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    var connection = request.accept('dashboard-stream', request.origin);
+    connectedClients.push(connection);
+    console.log((new Date()) + ' Connection accepted from ' + connection.remoteAddress + '. Connections: ' + connectedClients.length);
+    connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+        console.log('Dropping message: ' + message.utf8Data);
+      }
+    });
+    connection.on('close', function(reasonCode, description) {
+      connectedClients.splice(connectedClients.indexOf(connection), 1);
+      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected. Connections: ' + connectedClients.length);
+    });
+    publishData(connection);
+  } catch (exception) {
+    console.error(exception);
+  }
+});
+dashboard.onChange(function() {
+  connectedClients.forEach(function(connection) {
+    publishData(connection);
+  });
+});
 server.listen(port);
 console.log('HTTP Server started: http://localhost:' + port);
