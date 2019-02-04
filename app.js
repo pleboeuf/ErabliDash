387fde4b2493a64241e11ba513b3815920b51170
pleboeuf <pierre1.leboeuf@gmail.com>
2019-02-04 15:54:20 -0500

    Update many package for security reasons
    -Modified the .service file to use node v8.15.0
    -Now use node engine v8.15.0
    -Update to those modules:
        "express": "^4.16.4",
        "influx": "^5.0.7",
        "promise": "^8.0.2",
        "eslint": "^5.13.0",
        "eslint-config-standard": "^12.0.0",
        "eslint-plugin-import": "^2.16.0",
        "eslint-plugin-node": "^8.0.1",
        "eslint-plugin-promise": "^4.0.1",
        "eslint-plugin-standard": "^4.0.0"

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ dashboard.init().then(function () {
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(app.router);
-app.use(express.logger());
+// app.use(app.router);
+// app.use(express.logger());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.use('/', express.static(path.join(__dirname, 'index.html')));
