0b7805972dd500a54cae375ee2b955b0a674b289
Pierre Leboeuf <pierre1.leboeuf@gmail.com>
2023-11-02 15:14:57 -0400

    Add code for oper schematic and lines map
    - Add code files that display the operation schematic and vacuum lines map  that was not included.
    - This code was not written by me. It is included so its easier to install the code on a new environment.
    - Updated some node modules to a more recent version

---
+++ public/cabane2021/js/osmose.js
@@ -0,0 +1,360 @@
+/**
+ * http://usejsdoc.org/
+ * 
+ * <div class="divTable blueTable">
+<div class="divTableBody">
+<div class="divTableRow">
+<div class="divTableCell">cell1_1</div>
+<div class="divTableCell">cell2_1</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">cell1_2</div>
+<div class="divTableCell">cell2_2</div>
+</div>
+</div>
+</div>
+
+
+
+ "osmose": [
+	    {
+	      "code": "Osm1",
+	      "device": "EB-Osm1",
+	      "state": 1,
+	      "fonction": "   indefini  ",
+	     
+	      "alarmNo": 0,
+	      "alarmMsg": " - - - - - - - - - ",
+	      "lastUpdatedAt": "2021-02-28T16:04:01.992Z",
+	      "TempsOperEnCour": 300,
+	      "TempsSeq1234": 0,
+	      "TempsSeq4321": 0,
+	      "TempsDepuisLavage": 421,
+	      "startStopTime": 1614527944,
+	      "runTimeSec": 0,
+	      "PC_Conc": 0,
+	      "Conc_GPH": 0,
+	      "Filtrat_GPH": 0,
+	      "Total_GPH": 0
+	    }
+	  ]
+	  
+	  "osmose": [
+    {
+      "code": "Osm1",
+      "device": "EB-Osm1",
+      "state": 1,
+      "fonction": "   indefini  ",
+      "sequence": "?-?-?-?",
+      "alarmNo": 0,
+      "alarmMsg": " - - - - - - - - - ",
+      "lastUpdatedAt": "2021-02-28T16:56:39.763Z",
+      "TempsOperEnCour": 840,
+      "TempsSeq1234": 2503,
+      "TempsSeq4321": 0,
+      "TempsDepuisLavage": 3464,
+      "startStopTime": 1614530562,
+      "runTimeSec": 0,
+      "PC_Conc": 90,
+      "Conc_GPH": 330,
+      "Filtrat_GPH": 3000,
+      "Total_GPH": 3330,
+      "Col1": 16,
+      "Col2": 15,
+      "Col3": 12,
+      "Col4": 7,
+      "Conc": 5.5,
+      "Temp": 0.6,
+      "Pres": 275,
+      "BrixSeve": 1,
+      "BrixConc": 10
+    }
+  ]
+  "osmose": [
+    {
+      "code": "Osm1",
+      "device": "EB-Osm1",
+      "state": 1,
+      "fonction": "concentration",
+      "sequence": "1-2-3-4",
+      "alarmNo": 0,
+      "alarmMsg": " - - - - - - - - - ",
+      "lastUpdatedAt": "2021-02-28T17:29:39.668Z",
+      "TempsOperEnCour": 2820,
+      "TempsSeq1234": 5323,
+      "TempsSeq4321": 0,
+      "TempsDepuisLavage": 5444,
+      "startStopTime": 1614530562,
+      "runTimeSec": 1778,
+      "PC_Conc": 90,
+      "Conc_GPH": 330,
+      "Filtrat_GPH": 3000,
+      "Total_GPH": 3330,
+      "Col1": 17,
+      "Col2": 15,
+      "Col3": 10.5,
+      "Col4": 7,
+      "Conc": 5.5,
+      "Temp": 4.2,
+      "Pres": 300,
+      "BrixSeve": 1,
+      "BrixConc": 11
+    }
+  ]
+  
+   "osmose": [
+	  {
+		    "code": "Osm1",
+		    "device": "EB-Osm1",
+		    "state": 1,			 		
+		    "fonction": "concentration",
+		    "TempsOperEnCour": 60,  	
+		    "TempsSeq1234": 60,     		
+		    "TempsSeq4321": 0,      		
+		    "TempsDepuisLavage": 60, 	
+		    "lastUpdatedAt": "2018-03-23T20:44:49.517Z",		    
+		    "alarmNo": 0,	
+		     "alarmMsg": " - - - - - - - - - ", 
+		      
+		     "sequence": "?-?-?-?",
+		    "Col1": 13,
+		    "Col2": 11,
+		    "Col3": 9,
+		    "Col4": 7,
+		    "Conc": 8.5,
+		    "Temp": 6.5,
+		    "Pres": 300,
+		    "BrixSeve": 1.5,
+		    "BrixConc": 15.9,
+		    
+		    "PC_Conc": 82,				
+		    "Conc_GPH": 510,			
+		    "Filtrat_GPH": 2400,		
+		    "Total_GPH": 2910,			
+		    "Duree_sec": 68			
+
+		  }
+		]
+		
+		  "osmose": [
+	    {
+	      "code": "Osm1",
+	      "device": "EB-Osm1",
+	      "state": 1,
+	      "fonction": "   indefini  ",
+	      "sequence": "?-?-?-?",
+	      "alarmNo": 0,
+	      "alarmMsg": " - - - - - - - - - ",
+	      "lastUpdatedAt": "2021-02-28T16:51:39.779Z",
+	      "TempsOperEnCour": 540,
+	      "TempsSeq1234": 2503,
+	      "TempsSeq4321": 0,
+	      "TempsDepuisLavage": 3164,
+	      "startStopTime": 1614530562,
+	      "runTimeSec": 0,
+	      "PC_Conc": 90,
+	      "Conc_GPH": 330,
+	      "Filtrat_GPH": 3000,
+	      "Total_GPH": 3330,
+	      "Col1": 16,
+	      "Col2": 15,
+	      "Col3": 12,
+	      "Col4": 7,
+	      "Conc": 5.5,
+	      "Temp": 0.6,
+	      "Pres": 275,
+	      "BrixSeve": 1,
+	      "BrixConc": 10
+	    }
+	  ]
+
+	  
+	    "osmose": [														   "osmose": [                                      "osmose": [
+    {                                                                       {                                                  {
+      "code": "Osm1",                                                         "code": "Osm1",                                    "code": "Osm1",
+      "device": "EB-Osm1",                                                    "device": "EB-Osm1",                               "device": "EB-Osm1",
+      "state": 1,                                                             "state": 0,                                        "state": 1,
+      "fonction": "concentration",                                            "fonction": "concentration",                       "fonction": "   indefini  ",
+      "sequence": "4-3-2-1",                                                  "sequence": "4-3-2-1",                             "sequence": "?-?-?-?",
+      "alarmNo": 0,                                                           "alarmNo": 0,                                      "alarmNo": 0,
+      "alarmMsg": " - - - - - - - - - ",                                      "alarmMsg": " - - - - - - - - - ",                 "alarmMsg": " - - - - - - - - - ",
+      "lastUpdatedAt": "2021-03-01T16:17:50.231Z",                            "lastUpdatedAt": "2021-03-01T18:55:47.667Z",       "lastUpdatedAt": "2021-03-01T19:32:14.566Z",
+      "TempsOperEnCour": 2640,                                                "TempsOperEnCour": 1275,                           "TempsOperEnCour": 360,
+      "TempsSeq1234": 0,                                                      "TempsSeq1234": 0,                                 "TempsSeq1234": 0,
+      "TempsSeq4321": 10193,                                                  "TempsSeq4321": 14037,                             "TempsSeq4321": 0,
+      "TempsDepuisLavage": 18871,                                             "TempsDepuisLavage": 22715,                        "TempsDepuisLavage": 0,
+      "startStopTime": 1614612835,                                            "startStopTime": 1614624651,                       "startStopTime": 1614626780,
+      "runTimeSec": 122,                                                      "runTimeSec": 1275,                                "runTimeSec": 0,
+      "PC_Conc": 0,                                                           "PC_Conc": 90,                                     "PC_Conc": 0,
+      "Conc_GPH": 0,                                                          "Conc_GPH": 360,                                   "Conc_GPH": 0,
+      "Filtrat_GPH": 0,                                                       "Filtrat_GPH": 3240,                               "Filtrat_GPH": 0,
+      "Total_GPH": 0,                                                         "Total_GPH": 3600,                                 "Total_GPH": 0,
+      "Col1": 18,                                                             "Col1": 18,                                        "Col1": 18,
+      "Col2": 16,                                                             "Col2": 16,                                        "Col2": 16,
+      "Col3": 12,                                                             "Col3": 12,                                        "Col3": 12,
+      "Col4": 8,                                                              "Col4": 8,                                         "Col4": 8,
+      "Conc": 6,                                                              "Conc": 6,                                         "Conc": 6,
+      "Temp": 4.7,                                                            "Temp": 4.7,                                       "Temp": 4.7,
+      "Pres": 250,                                                            "Pres": 250,                                       "Pres": 250,
+      "BrixSeve": 1.2,                                                        "BrixSeve": 1.2,                                   "BrixSeve": 1.2,
+      "BrixConc": 10                                                          "BrixConc": 10                                     "BrixConc": 10
+    }                                                                       }                                                  }
+  ]                                                                       ]                                                  ]
+ "osmose": [
+    {
+      "code": "Osm1",
+      "device": "EB-Osm1",
+      "state": 0,
+      "fonction": "   indefini  ",
+      "sequence": "?-?-?-?",
+      "alarmNo": 0,
+      "alarmMsg": " - - - - - - - - - ",
+      "lastUpdatedAt": "2021-03-22T03:25:36.727Z",
+      "TempsOperEnCour": 13,
+      "TempsSeq1234": 0,
+      "TempsSeq4321": 0,
+      "TempsDepuisLavage": 2162,
+      "startStopTime": 1616362763,
+      "runTimeSec": 13,
+      "PC_Conc": 85,
+      "Conc_GPH": 480,
+      "Filtrat_GPH": 2670,
+      "Total_GPH": 3150,
+      "Col1": 15,
+      "Col2": 14,
+      "Col3": 10,
+      "Col4": 5.5,
+      "Conc": 8,
+      "Temp": 5,
+      "Pres": 290,
+      "BrixSeve": 1.1,
+      "BrixConc": 13
+    }
+  ]
+
+  Canada Canada	Canada	UN member state	CA	CAN	124	ISO 3166-2:CA	.ca
+*/
+		  osmose= 
+	    {
+	      "code": "Osm1",
+	      "device": "EB-Osm1",
+	      "state": 0,
+	      "fonction": "   indefini  ",
+	      "sequence": "?-?-?-?",
+	      "alarmNo": 0,
+	      "alarmMsg": " - - - - - - - - - ",
+	      "lastUpdatedAt": "2021-02-28T16:51:39.779Z",
+	      "TempsOperEnCour": 540,
+	      "TempsSeq1234": 2503,
+	      "TempsSeq4321": 0,
+	      "TempsDepuisLavage": 3164,
+	      "startStopTime": 1614530562,
+	      "runTimeSec": 0,
+	      "PC_Conc": 90,
+	      "Conc_GPH": 330,
+	      "Filtrat_GPH": 3000,
+	      "Total_GPH": 3330,
+	      "Col1": 16,
+	      "Col2": 15,
+	      "Col3": 12,
+	      "Col4": 7,
+	      "Conc": 5.5,
+	      "Temp": 0.6,
+	      "Pres": 275,
+	      "BrixSeve": 1,
+	      "BrixConc": 10
+	    };
+
+  osLabel  =                  
+                             {
+                               "code": "Osm1",
+                               "device": " ",            
+                               "state": " ",             
+                               "fonction": "Op\u00E9ration",          
+                               "sequence": "S\u00E9quence",          
+                               "alarmNo": "No Alarme ",           
+                               "alarmMsg": "Alarme",          
+                               "lastUpdatedAt": "Dern m.a.j.",     
+                               "TempsOperEnCour": "En cours depuis ",   
+                               "TempsSeq1234": "Temps Seq 1234 ",      
+                               "TempsSeq4321": "Temps Seq 4321 ",      
+                               "TempsDepuisLavage": "Temps depuis Lavage ", 
+                               "startStopTime": "D\u00E9part/fin ",     
+                               "runTimeSec": "Dur\u00E9e ",        
+                               "PC_Conc": "PC Conc",           
+                               "Conc_GPH": "Conc",          
+                               "Filtrat_GPH": "Filtrat",       
+                               "Total_GPH": "Total",         
+                               "Col1": "Col1",              
+                               "Col2": "Col2",              
+                               "Col3": "Col3",              
+                               "Col4": "Col4",              
+                               "Conc": "D\u00E9bit Conc.",              
+                               "Temp": "Temp\u00E9rature",              
+                               "Pres": "Pres",              
+                               "BrixSeve": "Brix S\u00E8ve",          
+                               "BrixConc": "Brix Conc"
+                               }
+                               ;
+ osUnit  =
+                             {
+                               "code": "",
+                               "device": "",            
+                               "state": "",             
+                               "fonction": "",          
+                               "sequence": "",          
+                               "alarmNo": "",           
+                               "alarmMsg": "",          
+                               "lastUpdatedAt": "",     
+                               "TempsOperEnCour": " min",   
+                               "TempsSeq1234": " min",      
+                               "TempsSeq4321": " min",      
+                               "TempsDepuisLavage": " min", 
+                               "startStopTime": " min",     
+                               "runTimeSec": " min",        
+                               "PC_Conc": " %",           
+                               "Conc_GPH": " g/h",          
+                               "Filtrat_GPH": " g/h",       
+                               "Total_GPH": " g/h",         
+                               "Col1": " g/h",              
+                               "Col2": " g/h",        
+                               "Col3":  " g/h",              
+                               "Col4":  " g/h",     
+                               "Conc": " g/h",              
+                               "Temp": " \u2103 ",              
+                               "Pres": " psi",              
+                               "BrixSeve": " Brix ",          
+                               "BrixConc": " Brix"
+                               }
+                               ;             
+function doit(){ 
+
+    var d = new Date(osmose.lastUpdatedAt);
+ //   d= Date.parse();
+    osmose.lastUpdatedAt = d.toLocaleTimeString();
+   
+	for (const [key, value] of Object.entries(osmose)) {
+		$('#'+key+'Label').html(osLabel[key]);
+		$('#'+key+'Value').html(value +osUnit[key]);
+
+
+	}		   
+	if(osmose.state==1) {
+			   osmose.stateValue = 
+			   $('#stateValue').html("ON");
+			   $(".titre").css("background","green");
+		   }else{
+			   $('#stateValue').html("OFF");
+			   $(".titre").css("background","white");
+		   } 
+}
+	/*
+	for (const [key, value] of Object.entries(os)) {
+		$('#'+key+'Value').html(value);
+	}
+	
+	*/
+	//	 $('#Pression_temperature').html(resultat2);   // met a jour le 
+	
+// Date.parse(no['lastUpdatedAt'])                        
+ 
